bc61054094c8d5e593a70f55c5d495c0dffb6f88
root <root@ip-172-31-34-178.ap-south-1.compute.internal>
2024-10-29 13:21:43 +0000

    privacy policy page

--- resources/views/web/ifream.blade.php
+++ resources/views/web/ifream.blade.php
@@ -2,7 +2,8 @@
 @section('content')
     <section>
         <div class="container game-ifream">
-            <iframe src="{{!empty($gameUrl) ? $gameUrl : '#' }}" scrolling="no" allowfullscreen="" width="1000" height="800" frameborder="0"></iframe>
+            <iframe src="{{!empty($gameUrl) ? $gameUrl : '#' }}" alt="" scrolling="no" allowfullscreen="" width="100%" height="100%" frameborder="0">
+            </iframe>
         </div>
     </section>
 @endsection
